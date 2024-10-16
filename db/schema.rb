acebe39f3be7809652407b41a62f701847f85071
Jesse Doyle <jdoyle@ualberta.ca>
2024-10-15 19:11:48 -0600

    deps: Update All Gems to Latest
    * Update rails to 7.2
    * All latest gems, hopefully no dependabot warnings.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2018_03_23_012305) do
+ActiveRecord::Schema[7.2].define(version: 2018_03_23_012305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,5 +19,4 @@ ActiveRecord::Schema[7.0].define(version: 2018_03_23_012305) do
     t.string "value"
     t.index ["instance_id"], name: "index_aliases_on_instance_id"
   end
-
 end
